f5f1c79b4f4eb9c0015d871c0752194916bb6f34
Kmos <amoskrr@gmail.com>
2019-08-10 09:01:04 +0300

    added location permission and location feature

--- app/src/main/java/com/imei/wifi_turn_on_off/MainActivity.java
+++ app/src/main/java/com/imei/wifi_turn_on_off/MainActivity.java
@@ -1,24 +1,34 @@
 package com.imei.wifi_turn_on_off;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+  private static final int MY_PERMISSIONS_REQUEST_LOCATION = 907;
   private WifiManager.LocalOnlyHotspotReservation mReservation;
   private Button turnOnButton;
   private Button turnOffButton;
   private Button turnOnHotspotButton;
   private Button turnOffHotspotButton;
-  private String TAG = getLocalClassName();
+  private String TAG = "Wifi";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -29,30 +39,34 @@ public class MainActivity extends AppCompatActivity {
     turnOnHotspotButton = findViewById(R.id.hotspoton);
     turnOffHotspotButton = findViewById(R.id.hotspotoff);
 
-    turnOnButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        turnOnWifi();
-      }
-    });
+    turnOnButton.setOnClickListener(v -> turnOnWifi());
 
-    turnOffButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        turnOffWifi();
-      }
-    });
+    turnOffButton.setOnClickListener(v -> turnOffWifi());
 
-    turnOnHotspotButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-          turnOnHotspot();
+    turnOnHotspotButton.setOnClickListener(v -> {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (checkLocationPermission()) {
+          if (isLocationEnabled()) {
+            turnOnHotspot();
+          } else {
+            redireTosetting();
+          }
+        } else {
+          requestLocationPermission();
         }
       }
     });
 
-    turnOffHotspotButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-          turnOffHotspot();
+    turnOffHotspotButton.setOnClickListener(v -> {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (checkLocationPermission()) {
+          if (isLocationEnabled()) {
+            turnOffHotspot();
+          } else {
+            redireTosetting();
+          }
+        } else {
+          requestLocationPermission();
         }
       }
     });
@@ -105,6 +119,76 @@ public class MainActivity extends AppCompatActivity {
   @RequiresApi(api = Build.VERSION_CODES.O) private void turnOffHotspot() {
     if (mReservation != null) {
       mReservation.close();
+    }
+  }
+
+  //request location permission the location hotspot requires location permission
+  private void requestLocationPermission() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+          Manifest.permission.ACCESS_FINE_LOCATION)) {
+        //Explanation
+        Toast.makeText(this, "rationale", Toast.LENGTH_SHORT).show();
+      } else {
+        // No explanation needed; request the permission
+        ActivityCompat.requestPermissions(this,
+            new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+            MY_PERMISSIONS_REQUEST_LOCATION);
+      }
+    } else {
+      // permission granted
+    }
+  }
+
+  private boolean checkLocationPermission() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // check for  location feature, for local hotspot to work , location must be on
+
+  public boolean isLocationEnabled() {
+    int locationMode = 0;
+    String locationProviders;
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      try {
+        locationMode =
+            Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE);
+      } catch (Settings.SettingNotFoundException e) {
+        e.printStackTrace();
+        return false;
+      }
+
+      return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+    } else {
+      locationProviders = Settings.Secure.getString(getContentResolver(),
+          Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+      return !TextUtils.isEmpty(locationProviders);
+    }
+  }
+
+  // redirect the user  to setting
+  public void redireTosetting() {
+    LocationManager locationManager =
+        (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+      AlertDialog alertDialog = new AlertDialog.Builder(this)
+          .setTitle("Location is turned off")  // GPS not found
+          .setMessage("Hot spot needs you location for it to work") // Want to enable?
+          .setPositiveButton("yes",
+              (dialogInterface, i) -> {
+                dialogInterface.dismiss();
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+              })
+          .setNegativeButton("No", (dialog, which) -> finish())
+          .setCancelable(false)
+          .show();
     }
   }
 }
